17056f96939ad73d1a2f3918c89b7b3c946b49d1
shuhan660 <hsu.shu.han.951023@gmail.com>
2026-07-06 09:07:58 +0800

    Remove with_for_update

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,22 @@
 from flask import Flask
 from config import Config
 from app.models import db
-from app.routes import main
 
 
 def create_app():
 
     app = Flask(__name__)
-
     app.config.from_object(Config)
 
     db.init_app(app)
 
-    app.register_blueprint(main)
-
     with app.app_context():
+
+        # 👉 延遲 import（重點）
+        from app.routes import main
+
+        app.register_blueprint(main)
+
         db.create_all()
 
     return app
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///database.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "sentencewebsite123"
